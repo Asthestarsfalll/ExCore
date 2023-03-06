8ba020e621133e3c3a3e36c0f40ad8c15be1a813
Asthestarsfalll <1186454801@qq.com>
2023-03-06 23:51:52 +0800

    :children_crossing: optimize example

--- example/run.py
+++ example/run.py
@@ -10,3 +10,5 @@ cfg = config.load("./run.toml")
 
 target_module = ["Model", "Optimizer", "Loss", "TrainData"]
 modules_dict, cfg_dict = config.build_all(cfg, target_module)
+logger.debug(modules_dict)
+logger.debug(cfg_dict)
