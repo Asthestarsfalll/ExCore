630f548bcab6e5ed185f8612b7b5ada33ee050b7
Asthestarsfalll <1186454801@qq.com>
2023-03-17 21:02:40 +0800

    :children_crossing: Update example

--- example/run.py
+++ example/run.py
@@ -1,7 +1,7 @@
 from prepare import MODELS
 
 from excore import Registry, config
-from excore.logger import logger
+from excore.logger import logger, add_logger
 
 
 def _check_func(values):
@@ -11,16 +11,22 @@ def _check_func(values):
     return False
 
 
+add_logger("file_log", "./run.log")
+
 # 打印 所有注册的 module 和 is_pretrained 的信息
 logger.info(MODELS.module_table(select_info=["is_pretrained"]))
 # 打印 所有注册的 module 和 is_backbone 的信息
 logger.info(MODELS.module_table(select_info=["is_backbone"]))
 # 打印 is_pretrained 为 true 的 module 和 is_backbone 的信息
 filtered_module_name = MODELS.filter("is_pretrained")
-logger.info(MODELS.module_table(select_info=["is_backbone"], module_list=filtered_module_name))
+logger.info(
+    MODELS.module_table(select_info=["is_backbone"], module_list=filtered_module_name)
+)
 # 打印 is_pretrained 为 true 的 module 和 is_pretrained 的信息
 filtered_module_name = MODELS.filter("is_pretrained", _check_func)
-logger.info(MODELS.module_table(select_info=["is_pretrained"], module_list=filtered_module_name))
+logger.info(
+    MODELS.module_table(select_info=["is_pretrained"], module_list=filtered_module_name)
+)
 logger.info(Registry.children_table())
 
 cfg = config.load("./run.toml")
