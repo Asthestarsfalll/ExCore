9d6a177d56b2eaa0805df9259cb3bb548d47b4bd
Asthestarsfalll <72954905+Asthestarsfalll@users.noreply.github.com>
2024-01-04 22:34:41 +0800

    :sparkles: Support non-instance value: class and post-hook (#18)

--- example/src/model/fcn.py
+++ example/src/model/fcn.py
@@ -1,10 +1,15 @@
 from src import MODEL
 
+IDX = 0  # for check
+
 
 @MODEL.register(is_pretrained=True)
 class FCN:
     def __init__(self, **kwargs):
-        pass
+        global IDX  # pylint: disable=global-statement
+
+        self.kwargs = kwargs
+        IDX += 1
 
     def parameters(self):
-        return "parameters of FCN"
+        return f"parameters of FCN_{IDX}"
--- example/src/optim/op.py
+++ example/src/optim/op.py
@@ -6,4 +6,5 @@ from excore.logger import logger
 @OPTIM.register()
 class AdamW:
     def __init__(self, **kwargs):
+        self.kwargs = kwargs
         logger.debug("AdamW kwargs: {}", kwargs)
